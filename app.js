c65b14ce3ab1ddb10dab9706cd2639c24c879a6e
Prabhjot Singh <prabhjotsingh394@gmail.com>
2023-03-27 15:30:07 +0530

    finished code

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('642144f8ba3f85f21d62fc61')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
