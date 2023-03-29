d5ac7085c291a906ea6f3c9f75d603beb53c8321
Prabhjot Singh <prabhjotsingh394@gmail.com>
2023-03-29 11:35:55 +0530

    Adding order now and displaying Orders

--- models/user.js
+++ models/user.js
@@ -28,7 +28,7 @@ const userSchema = new Schema({
   toJSON: { virtuals: true }
 });
 
-userSchema.methods.addToCart = function (product) {
+userSchema.methods.addToCart = function(product) {
   const cartProductIndex = this.cart.items.findIndex(cp => {
     return cp.productId.toString() === product._id.toString();
   });
@@ -51,13 +51,18 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
-userSchema.methods.removeFromCart = function (productId) {
+userSchema.methods.removeFromCart = function(productId) {
   const updatedCartItems = this.cart.items.filter(item => {
     return item.productId.toString() !== productId.toString();
   });
   this.cart.items = updatedCartItems;
   return this.save();
-}
+};
+
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
 
