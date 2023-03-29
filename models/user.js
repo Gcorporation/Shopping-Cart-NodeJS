342a27eb84f6ef6dfff410b5ec865454d714200a
Prabhjot Singh <prabhjotsingh394@gmail.com>
2023-03-29 11:21:06 +0530

    deleting cart items

--- models/user.js
+++ models/user.js
@@ -23,12 +23,12 @@ const userSchema = new Schema({
       }
     ]
   }
-},{
-    timestamps: true,
-    toJSON: {virtuals: true}
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true }
 });
 
-userSchema.methods.addToCart = function(product) {
+userSchema.methods.addToCart = function (product) {
   const cartProductIndex = this.cart.items.findIndex(cp => {
     return cp.productId.toString() === product._id.toString();
   });
@@ -50,6 +50,14 @@ userSchema.methods.addToCart = function(product) {
   this.cart = updatedCart;
   return this.save();
 };
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 
